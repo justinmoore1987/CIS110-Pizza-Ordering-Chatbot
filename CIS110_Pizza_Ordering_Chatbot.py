9878e8e9a610dc5d38533debdf2a0a6765ce1bc7
username <useremail>
2024-04-11 01:43:39 -0700

    Added Date Validation While loops.

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,16 +1,35 @@
+#Design a pizza ordering chatbot for users to build a pizza order, specifying size, flavor, crust type, quantity, method to receive and also displays accurate price.*****
 print("Hello, my name is Alex your virtual assistant. I will help you order a pizza!")
 print("I am going to ask you a few questions. After typing an answer, press enter.")
+#User enters name, field cannot be left blank, identify me as creator.*****
 userName = input("\nEnter your name:  ")
+while len(userName) == 0:
+    userName = input("Name cannot be blank! Please enter your name:  ")
 if userName.lower() == "justin":
     print(f"\nMy creator, {userName}. Pleasure to serve you!")
 else:
     print(f"\nHello, {userName}. Nice to meet you!")
+#User enters size, field cannot be left blank, entry must be recognized by application.*****
 size = input("\nWhat size do you want? Enter small, medium, or large:  ")
+while size.lower() not in ["small", "medium", "large"]:
+    size = input("Invalid value! Please enter small, medium, or large:  ")
+#User enters flavor, field cannot be left blank.*****
 flavor = input("\nEnter the flavor of pizza:  ")
+while len(flavor) == 0:
+    flavor = input("Flavor cannot be blank! Please enter a flavor:  ")
+#User enters crust type, field cannot be left blank.*****
 crustType = input("\nWhat type of crust do you want:  ")
+while len(crustType) == 0:
+    crustType = input("Crust type cannot be blank! Please enter crust type:  ")
+#Eser enters quantity, field must be a numeric value.*****
 quantity = input("\nHow many of these do you want to order? Enter a numeric value:  ")
+while not quantity.isdigit():
+    quantity = input("\nValue not recognized. Please enter a numeric value:  ")
 quantity = int(quantity)
+#User enters method for receiving order, entry must be recognized by application, for delivery orders add $5 fee.*****
 method = input("\nIs this carry out or delivery:  ")
+while method not in ["carry out", "delivery"]:
+    method = input("Invalid value! Please enter carry out or delivery:  ")
 if method.lower() == "delivery":
     deliveryFee = 5
 else:
